72489f7dc347f3eadcfb65d0d27253b68ed8ccfa
FernandaSena07 <fernanda.sena0709@gmail.com>
2023-08-29 15:44:24 -0300

    atualiza_pastas&add_Comentarios

--- index.js
+++ index.js
@@ -1,129 +1,142 @@
-let previousNumber = ""
-let nextNumber = ""
-let operation = ""
+let numeroInicial = "" //primeiro numero clicado//
+let proximoNumero = "" //próximo numero clicado//
+let operacao = "" //operacao selecionada//
 
 
 function calculate() {
-  previousNumber = Number(previousNumber.replace(/,/g, ""))
-  nextNumber = Number(nextNumber.replace(/,/g, ""))
+  numeroInicial = Number(numeroInicial.replace(/,/g, "")) //formata o dado de string para number//
+  proximoNumero = Number(proximoNumero.replace(/,/g, ""))
 
-  switch (operation) {
+  switch (operacao) { //Estrutura caso escolha para operações//
     case "+":
-      previousNumber = previousNumber + nextNumber
-      break
+      numeroInicial = numeroInicial + proximoNumero //resultado sendo armazenado no Numero inicial//
+      break //final do caso
     
     case "-":
-      previousNumber = previousNumber - nextNumber
+      numeroInicial = numeroInicial - proximoNumero
       break
     
     case "x":
-      previousNumber = previousNumber * nextNumber
+      numeroInicial = numeroInicial * proximoNumero
       break
     
     case "÷":
-      previousNumber = previousNumber / nextNumber
+      numeroInicial = numeroInicial / proximoNumero
       break
     
-    default:
+    default: //será exec utado quando nenhuma das opções seja selecionada. //
       break
   }
 
-  operation = ""
-  nextNumber = ""
+  operacao = "" // zera o valor//
+  proximoNumero = "" // zera o valor//
 }
 
 function formatNumber(number) {
   number = number.toString()
-
-  const integerDigits = Number(number.split(".")[0].replace(/,/g, ""))
+//formata o retorno da variavel dividindo o numeo com "." ou "," //
+  const integerDigits = Number(number.split(".")[0].replace(/,/g, "")) //substitui todas as virgulas por string vazia//
   const decimalDigits = number.split(".")[1]
 
+  //verifica se é numero decimal, se a variável é inteira para realizar sozinha a formatação.//
   if (decimalDigits === undefined) {
     number = integerDigits.toLocaleString("en", {
-      maximumFractionDigits: 0
+      maximumFractionDigits: 0 //Determinando que o primeiro valor não tenha valores decimais zerados.//
     })
   } else {
-    number = `${integerDigits.toLocaleString("en", {
+    number = `${integerDigits.toLocaleString("en", { 
       maximumFractionDigits: 0
-    })}.${decimalDigits}`
+    })}.${decimalDigits}` //Adiciona o . de separação da parte inteira. //
   }
 
   return number
 }
 
 function updateScreen() {
-  if (operation === "") {
-    previousNumber = formatNumber(previousNumber)
-    $exibeResposta.innerHTML = previousNumber
+  if (operacao === "") {
+    numeroInicial = formatNumber(numeroInicial) //primeiro numero recebe a função formatar numero atualizando assim a resposta //
+    $exibeResposta.innerHTML = numeroInicial //exibe a resposta final no html.//
 
   } else {
-    previousNumber = formatNumber(previousNumber)
+    numeroInicial = formatNumber(numeroInicial)
 
-    if (nextNumber !== "") {
-      nextNumber = formatNumber(nextNumber)
+    if (proximoNumero !== "") {
+      proximoNumero = formatNumber(proximoNumero)
     }
 
-    $exibeResposta.innerHTML = `${previousNumber} ${operation} ${nextNumber}`
+    $exibeResposta.innerHTML = `${numeroInicial} ${operacao} ${proximoNumero}` //exibe valores na tela html//
   }
 }
 
 function appendNumber(number) {
-  if (operation === "") {
-    if (previousNumber.includes(".") && number === ".") return
-    previousNumber += number
+  //verifica se esta puxando o primeiro ou o próximo numero. Se a string estiver vazia está lidando com o numero incial. senão é o próximo numero//
+  if (operacao === "") {
+    if (numeroInicial.includes(".") && number === ".") return ////
+    numeroInicial += number
 
   } else {
-    if (nextNumber.includes(".") && number === ".") return
-    nextNumber += number
+    //verifica se o próximo numero já tem casa decimal, não permitindo que coloque mais. //
+    if (proximoNumero.includes(".") && number === ".") return
+    proximoNumero += number //ela mais ela mesma e o botão clicado pelo usuário//
   }
 
   updateScreen()
 }
 
 
-function chooseOperation(operator) {
-  if (operation !== "" && nextNumber !== "") {
+function escolherOperacao(operator) {
+  //verifica se a operação esta vazia ou o próximo valor esta vazio para realizar o calculo//
+  if (operacao !== "" && proximoNumero !== "") {
     calculate()
   }
 
-  operation = operator
+  operacao = operator
   updateScreen()
 }
 
+//Reinicia a calculadora, zera as variaáveis//
 function clearAll() {
-  previousNumber = ""
-  nextNumber = ""
-  operation = ""
-  $exibeResposta.innerHTML = "0"
+  numeroInicial = ""
+  proximoNumero = ""
+  operacao = ""
+  $exibeResposta.innerHTML = "0" //Volta a aparecer o zero no exibe resposta na tela//
 }
 
+//Apaga o ultimo digito//
 function deleteDigit() {
-  if (operation === "") {
-    previousNumber = previousNumber.slice(0, -1)
+  //Verifica se está chamando o primerio numero digitado ou o ultimo numero. Para retirar o ultimo caracter slice por ser string//
+  if (operacao === "") {
+    numeroInicial = numeroInicial.slice(0, -1) //Se a operaçao estiver vazia, esta trabalhando com o numero inicial//
   } else {
-    nextNumber = nextNumber.slice(0, -1)
+    proximoNumero = proximoNumero.slice(0, -1) //Senão está trabalhando com o último numero digitado. //
   }
 
-  updateScreen()
+  updateScreen()//chama a fuçao atualiza a tela da calculadora//
 }
 
 
 const $exibeResposta = document.querySelector(".exibeResposta")
-
+//Usa-se o "$"(não obrigatório) caso seja criado uma variável com o mesmo nome que já foi criado no html. Cria uma função para todos os botões com data-number //
 const $numberButtons = document.querySelectorAll("[data-number]")
-$numberButtons.forEach(button => button.addEventListener("click", () => appendNumber(button.dataset.number)))
+//Adiciona o evento click, que significa que toda vez que o botão for clicado acontecerá algo// 
+$numberButtons.forEach(button => button.addEventListener("click", () => appendNumber(button.dataset.number)))//usa-se o dataset para achar o elemento clicado e .number para saber qual numero foi clicado//
+//Adiciona o evento click, que significa que toda vez que o botão for clicado acontecerá algo//
 
 const $operatorButtons = document.querySelectorAll("[data-operator]")
-$operatorButtons.forEach(button => button.addEventListener("click", () => chooseOperation(button.dataset.operator)))
+$operatorButtons.forEach(button => button.addEventListener("click", () => escolherOperacao(button.dataset.operator)))//usa-se o dataset para achar o elemento clicado e .number para saber qual operador foi clicado//
 
+//Adiciona ouvidor do evento click ao botão de igual//
 const $equalButton = document.querySelector("[data-equal]")
 $equalButton.addEventListener("click", () => {
-  calculate()
-  updateScreen()
+  calculate() //a operação calcular é chamada//
+  updateScreen() //a operação atualizar resultado é chamada//
 })
 
+//Adiciona o evento click, quando clicado a função clearAll() é acionada//
 const $clearAllButton = document.querySelector("[data-clear-all]")
 $clearAllButton.addEventListener("click", () => clearAll())
 
+//chama a função//
 const $deleteButton = document.querySelector("[data-delete]")
+//Adiciona o ouvidor do evento click,quando clicado apaga o ultimo digito//
 $deleteButton.addEventListener("click", () => deleteDigit())
